3ccc674add757ec2df785321ff2f5762d3108d49
Ika rahma alia <ika418471@gmail.com>
2023-12-21 11:14:04 +0700

    style transaksi

--- app/Views/dashboard_cust.php
+++ app/Views/dashboard_cust.php
@@ -46,6 +46,14 @@
                         </a>
                      </li>
                      <li>
+                        <a href="<?= base_url('/riwayat_transaksi') ?>" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
+                           <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path>
+                           </svg>
+                           <span class="ml-3 flex-1 whitespace-nowrap">Transaksi</span>
+                        </a>
+                     </li>
+                     <li>
                         <a href="<?= url_to('logout') ?>" class="text-base text-gray-900 font-normal rounded-lg hover:bg-gray-100 flex items-center p-2 group ">
                          <?= csrf_field() ?>
                           <svg class="w-6 h-6 text-gray-500 flex-shrink-0 group-hover:text-gray-900 transition duration-75" fill="currentColor" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -94,6 +102,7 @@
             <span class="text-3xl font-bold text-gray-900 dark:text-white">$<?= $produk['harga'] ?></span>
             <a href="<?= base_url('beli/' . $produk['produk_id']) ?>" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Beli</a>
          </div>
+         <br>
 			<div class="flex items-center mt-2.5 mb-5">
 							<svg class="w-5 h-5 text-yellow-300" fill="currentColor" viewBox="0 0 20 20"
 								xmlns="http://www.w3.org/2000/svg">
@@ -129,25 +138,15 @@
 						</div>
 						<div class="flex items-center justify-between">
 							<span class="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-							<a href="#"
-								class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
-								to cart</a>
+							
 						</div>
-                  <a href="<?= base_url('dash_cust/' . $produk['produk_id'] . '/edit_produk') ?>">
-                     <button class="btn btn-warning"><i class="fa fa-pen" style="color: white;"></i></button>
-                  </a>
-                  <form action="<?= base_url('dash_cust/' . $produk['produk_id']) ?>" method="post" style="display: inline-block">
-                     <input type="hidden" name="_method" value="delete">
-                     <?= csrf_field() ?>
-                     <a>
-                        <button type="submit" class="btn btn-danger" onclick="return confirmDelete();"><i class="fa fa-trash"></i></button>
-                     </a>
+                  
                   </form>
+		   </div>
 		</div>
-		</div>
-	<?php } ?>
-	</div>
-	</div>  
+            <?php } ?>
+            </div>
+            </div>  
 </div>
 
          </main>
